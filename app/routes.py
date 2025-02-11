e855b2bb522df549fcb30b87046987a4aa21e7b6
jack_gullo <john.gullo@bobcats.gcsu.edu>
2025-02-11 13:35:10 -0500

    first

--- app/routes.py
+++ app/routes.py
@@ -9,3 +9,22 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/hank_aaron')
+def hank_aaron():
+    return render_template('hank_aaron.html')
+
+@app.route('/chipper_jones')
+def chipper_jones():
+    return render_template('chipper_jones.html')
+
+@app.route('/greg_maddux')
+def greg_maddux():
+    return render_template('greg_maddux.html')
+
+@app.route('/freddie_freeman')
+def freddie_freeman():
+    return render_template('freddie_freeman.html')
+
+@app.route('/ronald_acuna')
+def ronald_acuna():
+    return render_template('ronald_acuna.html')
